9663db1750ed46ae5f482da404fda7443c93be18
cijokb <cijo.kb@gmail.com>
2019-09-11 11:46:35 +0530

    Added firebase deployment

---
+++ build/precache-manifest.66b59dfc35fc7df976f5cbf543387043.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "953217ac6c659b10b399699abe7c7c5b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eb967d14a3ec653c5698",
+    "url": "/static/css/main.4be8e21e.chunk.css"
+  },
+  {
+    "revision": "0023dbd3b4554efd0175",
+    "url": "/static/js/2.809eba2a.chunk.js"
+  },
+  {
+    "revision": "eb967d14a3ec653c5698",
+    "url": "/static/js/main.0025c9d6.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/static/media/burger-logo.b8503d26.png"
+  }
+]);
